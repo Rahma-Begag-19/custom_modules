829f027de47bf64352fa57c8f53c0fe6dbec0c96
Rahma Begag <rahmabegag19@gmail.com>
2025-08-06 15:46:07 +0200

    password visibility model

--- password_visibility/__manifest__.py
+++ password_visibility/__manifest__.py
@@ -8,6 +8,8 @@
         This module adds an eye icon to the Odoo 17 login page, allowing users to toggle password visibility.
     """,
     'author': 'Rahma BEGAG',
+    'website': 'https://www.linkedin.com/in/rahma-begag-b49a70349/',
+    'license': 'LGPL-3',
     'depends': ['web', 'auth_signup'],
     'data': [
         'views/web_templates.xml',
